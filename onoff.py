5e3aaaa50aaea8fcb45fc11c5103be0bd7ed7f2a
J Rowley <justin.rowley@mail.bcu.ac.uk>
2018-07-30 18:46:08 +0100

    This update is so I can test the web app without needing my Pi, see description for added features
    Web Server detects whether PC is on or off and loads the HTML page.
    
    Thats about it right now

--- onoff.py
+++ onoff.py
@@ -1,9 +1,11 @@
-import RPi.GPIO as GPIO, time
+#import RPi.GPIO as GPIO, 
+import time
 
 #change these variables to adjust which GPIO pin will be the SwitchPin for switching the PC on or off
 SwitchPin = 2
 #change these variables to adjust which GPIO pin will be the the PowerLED pin for detecting if the PC is on or not
 PowerLEDPin = 3  
+
 
 def setup():
     GPIO.setmode(GPIO.BCM)
@@ -12,26 +14,33 @@ def setup():
     GPIO.setup(PowerLEDPin, GPIO.IN)
 
 def on():
+    '''
     setup()
     time.sleep(0.5)
     GPIO.output(SwitchPin, GPIO.HIGH)
     time.sleep(0.5)
     GPIO.ouput(SwitchPin, GPIO.LOW)
     GPIO.cleanup()
-    return None
+    '''
+    return "on"
 
 def off():
+    '''
     setup()
     time.sleep(0.5)
     GPIO.output(SwitchPin, GPIO.HIGH)
     time.sleep(5)
     GPIO.output(SwitchPin, GPIO.LOW)
     GPIO.cleanup()
-    return None
+    '''
+
+    return "off"
 
 def detect():
-    setup()
+    '''setup()
     test = GPIO.read(PowerLEDPin)
+    '''
+    test = True
     if test == True:
         PC = "on"
     elif test == False:
