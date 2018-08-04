7bcf8ac904271af7355d1fcdcbcd0d02b46e3a1b
JR <justin.rowley@mail.bcu.ac.uk>
2018-08-04 14:04:17 +0100

    Making changes for testing

--- onoff.py
+++ onoff.py
@@ -6,41 +6,36 @@ SwitchPin = 2
 #change these variables to adjust which GPIO pin will be the the PowerLED pin for detecting if the PC is on or not
 PowerLEDPin = 3  
 
-'''
 def setup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     GPIO.setup(SwitchPin, GPIO.OUT, initial=GPIO.LOW)
     GPIO.setup(PowerLEDPin, GPIO.IN)
-'''
+
 
 def on():
-    '''
+    
     setup()
     time.sleep(0.5)
     GPIO.output(SwitchPin, GPIO.HIGH)
     time.sleep(0.5)
     GPIO.ouput(SwitchPin, GPIO.LOW)
     GPIO.cleanup()
-    '''
     return "on"
 
 def off():
-    '''
     setup()
     time.sleep(0.5)
     GPIO.output(SwitchPin, GPIO.HIGH)
     time.sleep(5)
     GPIO.output(SwitchPin, GPIO.LOW)
     GPIO.cleanup()
-    '''
-
     return "off"
 
 def detect():
-    '''setup()
+    setup()
     test = GPIO.read(PowerLEDPin)
-    '''
+    
     test = True
     if test == True:
         PC = "on"
